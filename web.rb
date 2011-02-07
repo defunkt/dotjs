0c716b45fc6947dde9e00a09d74599f929be3864
Chris Wanstrath <chris@ozmm.org>
2011-02-06 22:53:44 -0800

    first draft

---
+++ web.rb
@@ -0,0 +1,11 @@
+# $ ruby -rubygems web.rb -p 3131
+require 'sinatra'
+
+get '/:host' do
+  content_type 'text/plain'
+  headers 'Access-Control-Allow-Origin' => '*'
+  host = params[:host].gsub('..', '.').gsub('www.','')
+  if File.exist? file = File.expand_path("~/.js/#{host}.js")
+    File.read(file)
+   end
+end
