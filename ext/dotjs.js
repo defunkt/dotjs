3c98ffcfb21d73901210f7109334f5e7b6e74e0f
Chris Wanstrath <chris@ozmm.org>
2011-02-21 20:56:00 -0800

    Always load a default.js if it exists.

--- ext/dotjs.js
+++ ext/dotjs.js
@@ -1,9 +1,11 @@
-$.ajax({
-  url: 'http://localhost:3131/'+window.location.host.replace('www.','')+'.js',
-  success: function(d){
-    $(function(){ eval(d) })
-  },
-  error: function(){
-    console.log('no dotjs server found at localhost:3131')
-  }
+$.each(['default', window.location.host.replace('www.','')], function(index, name) {
+  $.ajax({
+    url: 'http://localhost:3131/'+name+'.js',
+    success: function(d){
+      $(function(){ eval(d) })
+    },
+    error: function(){
+      console.log('no dotjs server found at localhost:3131')
+    }
+  })
 })
