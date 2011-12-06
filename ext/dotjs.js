4b4e625439554f696b18f557a451c1668f1458b0
Bart Teeuwisse <bart.teeuwisse@thecodemill.biz>
2011-12-05 23:24:04 -0800

    - Load <domain>.css upon page load.

--- ext/dotjs.js
+++ ext/dotjs.js
@@ -1,4 +1,14 @@
 $.ajax({
+  url: 'http://localhost:3131/'+window.location.host.replace('www.','')+'.css',
+  dataType: 'text',
+  success: function(d) {
+    $('head').prepend('<style type="text/css">' + d + '</style>')
+  },
+  error: function(){
+    console.log('no dotjs server found at localhost:3131')
+  }
+});
+$.ajax({
   url: 'http://localhost:3131/'+window.location.host.replace('www.','')+'.js',
   dataType: 'text',
   success: function(d){
