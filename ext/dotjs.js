5a8c946e75506760ee04c9834f3691cb23528715
Ahmed Nuaman <ahmed@ahmednuaman.com>
2015-07-21 13:02:56 +0100

    removed jquery

--- ext/dotjs.js
+++ ext/dotjs.js
@@ -1,10 +1,17 @@
-$.ajax({
-  url: 'https://localhost:3131/'+location.hostname.replace(/^www\./,'')+'.js',
-  dataType: 'text',
-  success: function(d){
-    $(function(){ eval(d) })
-  },
-  error: function(){
-    console.log('no dotjs server found at localhost:3131')
+var xhr = new XMLHttpRequest();
+
+xhr.onload = function (event) {
+  documentReady(this.responseText);
+}
+
+xhr.open('get', 'https://localhost:3131/'+location.hostname.replace(/^www\./,'')+'.js', true);
+xhr.responseType = 'text';
+xhr.send();
+
+function documentReady (responseType) {
+  if (!document.body) {
+    setTimeout(documentReady, 10, responseText);
+  } else {
+    eval(responseText);
   }
-})
+}
